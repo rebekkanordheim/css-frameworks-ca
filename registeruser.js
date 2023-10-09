0ad38530bbbfedb055330a00cf2a62ea641cf82c
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-10-09 18:13:45 +0200

    registeruser done

--- registeruser.js
+++ registeruser.js
@@ -1,9 +1,44 @@
 const API_BASE_URL = 'https://nf-api.onrender.com';
 
-// Endpoints
-// Register: /api/v1/social/auth/login - CURRENT
+// End points
+// Register: /api/v1/social/auth/register
 
-// Login: /api/v1/social/auth/login
-// Get all posts: /api/v1/social/posts
+//------------ Registers User 
+/**
+ * API call that registers the user
+ * @param {string} url 
+ * @param {any} userData 
+ * ```js
+ * registerUser(registerURL, userToRegister);
+ * ```
+ */
 
-// Email validation function
+async function registerUser(url, userData) {
+    console.log(url, userData);
+    try {
+        // do API call
+        const postData = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        };
+        const response = await fetch(url, postData);
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const userToRegister = {
+    name: 'rebekka_nordheim',
+    email: 'rebekka_nordheim@stud.noroff.no',
+    password: 'secure123',
+};
+
+const registerURL = `${API_BASE_URL}/api/v1/social/auth/register`;
+
+registerUser(registerURL, userToRegister);
